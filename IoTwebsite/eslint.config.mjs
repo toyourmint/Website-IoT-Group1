000de1a0d612148601116c1ad9354cad60cc7bf6
natsu <litae2545@gmail.com>
2026-03-02 15:00:49 +0700

    Update eslint.config.mjs

--- IoTwebsite/eslint.config.mjs
+++ IoTwebsite/eslint.config.mjs
@@ -3,8 +3,8 @@ import withNuxt from './.nuxt/eslint.config.mjs'
 export default withNuxt({
   rules: {
     // ปิดการแจ้งเตือนเรื่องชื่อ Component ต้องมี 2 คำ
-    'vue/multi-word-component-names': 'off',
-    
+    'vue/multi-word-component-names': 'off'
+
     // ถ้ามีกฎอื่นที่อยากปิดอีก ก็เพิ่มลงมาตรงนี้ได้เลย เช่น
     // 'no-console': 'off' (อนุญาตให้ใช้ console.log ได้)
   }
