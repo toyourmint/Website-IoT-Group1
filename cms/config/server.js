54ca563c65f099f798a64fa9db628065f3fdf5d6
ัYadawadee Klinsmith <72minniemy@gmail.com>
2026-03-04 16:14:40 +0700

    Update server.js

--- cms/config/server.js
+++ cms/config/server.js
@@ -1,9 +1,14 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+
+  url: 'https://67011430.consolutechcloud.com',
+  proxy: true,
+
   app: {
     keys: env.array('APP_KEYS'),
   },
+
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
   },
